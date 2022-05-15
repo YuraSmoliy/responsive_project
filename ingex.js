ae754e246061fe5fec393ff33871c7aef1204af5
smolii <yuriy.smolii@nixsolutions.com>
2022-05-15 23:40:08 +0300

    initial commit

---
+++ ingex.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  const elem = document.getElementById("opened-menu");
+  elem.style = "display:block";
+}
+
+function closeMenu() {
+  const elem = document.getElementById("opened-menu");
+  elem.style = "display:none";
+}
